967d71166ddd0f0a3924f47c4ba5c2a40bab270b
herve beraud <herveberaud.pro@gmail.com>
2016-12-02 23:29:33 +0100

    [fix] rename file

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,7 +4,7 @@ set -e
 if [[ $TRAVIS_BRANCH == 'master' ]] ; 
 then
     mv README.md index.md
-    mv CONTRIBUTORS contributors.md
+    mv CONTRIBUTORS.md contributors.md
     mv LICENCE licence.md
     git init
     git config user.name "Hervé Beraud"
