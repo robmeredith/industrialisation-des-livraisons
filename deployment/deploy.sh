24864d3dfa39c2788a4e4064d63e509c482934a5
herve beraud <herveberaud.pro@gmail.com>
2016-12-02 23:17:30 +0100

    [fix] git publish

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,19 +1,21 @@
-language: ruby
-rvm: 
-    - 2.2.5
+#!/bin/bash
 
-install: gem install jekyll html-proofer jekyll-sitemap
+set -e
+if [[ $TRAVIS_BRANCH == 'master' ]] ; 
+then
+    mv README.md index.md
+    mv CONTRIBUTORS contributors.md
+    mv LICENCE licence.md
+    git init
+    git config user.name "Hervé Beraud"
+    git config user.email "herveberaud.pro@gmail.com"
+    git add .
+    git status
+    git commit -m "[site][v0.3.0] automatic deployment"
+    git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:gh-pages > /dev/null 2>&1
+    echo "gh-pages updated"
+else
+    echo 'Invalid branch. You can only deploy from master.'
+    exit 1
+fi
 
-sudo: required
-
-env:
-    global:
-        - NOKOGIRI_USE_SYSTEM_LIBRARIES=true
-
-script:
-    - rm -rf .git
-    - jekyll new .
-
-after_success:
-    - chmod +x -R deployment
-    - deployment/deploy.sh
